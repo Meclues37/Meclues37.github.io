1ded65ead721fa9f37e8a12dfb37a10d74180e41
Meclues37 <yisenxiong@gmail.com>
2026-04-25 05:37:49 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log("Website loaded");
+console.log("Photography portfolio loaded.");
+
+const cards = document.querySelectorAll(".photo-card, .place-card");
+
+cards.forEach(card => {
+  card.addEventListener("mouseenter", () => {
+    card.style.cursor = "pointer";
+  });
+});
